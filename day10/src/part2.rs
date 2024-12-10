4860c41625761d1cae32f35fba45a8eb943a29f1
Andrew Howard <andrew@adh.me.uk>
2024-12-10 14:33:15 +0000

    Memoise part 2

--- day10/src/part2.rs
+++ day10/src/part2.rs
@@ -4,6 +4,8 @@ pub fn solve(input: &str) -> usize {
         .map(|line| line.as_bytes())
         .collect::<Vec<_>>();
 
+    let mut memo = vec![vec![None; map[0].len()]; map.len()];
+
     map.iter()
         .enumerate()
         .flat_map(|(row, data)| {
@@ -17,13 +19,19 @@ pub fn solve(input: &str) -> usize {
                 },
             )
         })
-        .map(|(row, col)| walk_trails(&map, row, col, b'0'))
+        .map(|(row, col)| walk_trails(&map, row, col, b'0', &mut memo))
         .sum()
 }
 
 const DIRECTIONS: [(isize, isize); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
 
-fn walk_trails(map: &[&[u8]], row: usize, col: usize, height: u8) -> usize {
+fn walk_trails(
+    map: &[&[u8]],
+    row: usize,
+    col: usize,
+    height: u8,
+    memo: &mut Vec<Vec<Option<usize>>>,
+) -> usize {
     if height == b'9' {
         1
     } else {
@@ -40,7 +48,13 @@ fn walk_trails(map: &[&[u8]], row: usize, col: usize, height: u8) -> usize {
                 let Some(data) = map.get(row) else { return 0 };
                 let Some(cell) = data.get(col) else { return 0 };
                 if *cell == next_height {
-                    walk_trails(map, row, col, next_height)
+                    if let Some(n) = memo[row][col] {
+                        n
+                    } else {
+                        let n = walk_trails(map, row, col, next_height, memo);
+                        memo[row][col] = Some(n);
+                        n
+                    }
                 } else {
                     0
                 }
