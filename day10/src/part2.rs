412bb572df7729a75dfccaa3aedd5beae06bbf33
Andrew Howard <andrew@adh.me.uk>
2024-12-10 21:02:43 +0000

    Merge memo into main map

--- day10/src/part2.rs
+++ day10/src/part2.rs
@@ -1,37 +1,42 @@
+use std::cell::Cell;
+
+struct Position {
+    height: u8,
+    rating: Cell<Option<usize>>,
+}
+
 pub fn solve(input: &str) -> usize {
     let map = input
         .lines()
-        .map(|line| line.as_bytes())
-        .collect::<Vec<_>>();
-
-    let mut memo = vec![vec![None; map[0].len()]; map.len()];
+        .map(|line| {
+            line.as_bytes()
+                .iter()
+                .map(|&height| Position {
+                    height,
+                    rating: Cell::new(None),
+                })
+                .collect()
+        })
+        .collect::<Vec<Vec<_>>>();
 
     map.iter()
         .enumerate()
         .flat_map(|(row, data)| {
-            data.iter().enumerate().filter_map(
-                move |(col, &cell)| {
-                    if cell == b'0' {
-                        Some((row, col))
-                    } else {
-                        None
-                    }
-                },
-            )
+            data.iter().enumerate().filter_map(move |(col, cell)| {
+                if cell.height == b'0' {
+                    Some((row, col))
+                } else {
+                    None
+                }
+            })
         })
-        .map(|(row, col)| walk_trails(&map, row, col, b'0', &mut memo))
+        .map(|(row, col)| walk_trails(&map, row, col, b'0'))
         .sum()
 }
 
 const DIRECTIONS: [(isize, isize); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
 
-fn walk_trails(
-    map: &[&[u8]],
-    row: usize,
-    col: usize,
-    height: u8,
-    memo: &mut Vec<Vec<Option<usize>>>,
-) -> usize {
+fn walk_trails(map: &Vec<Vec<Position>>, row: usize, col: usize, height: u8) -> usize {
     if height == b'9' {
         1
     } else {
@@ -45,14 +50,15 @@ fn walk_trails(
                 let Some(col) = col.checked_add_signed(*dc) else {
                     return 0;
                 };
-                let Some(data) = map.get(row) else { return 0 };
-                let Some(cell) = data.get(col) else { return 0 };
-                if *cell == next_height {
-                    if let Some(n) = memo[row][col] {
+                let Some(position) = map.get(row).and_then(|row| row.get(col)) else {
+                    return 0;
+                };
+                if position.height == next_height {
+                    if let Some(n) = position.rating.get() {
                         n
                     } else {
-                        let n = walk_trails(map, row, col, next_height, memo);
-                        memo[row][col] = Some(n);
+                        let n = walk_trails(map, row, col, next_height);
+                        position.rating.set(Some(n));
                         n
                     }
                 } else {
