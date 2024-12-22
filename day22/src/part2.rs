2dfac58492e54730f19792e8c781b29754d5651f
Andrew Howard <andrew@adh.me.uk>
2024-12-22 17:38:42 +0000

    Implemented day 22 part 2

--- day22/src/part2.rs
+++ day22/src/part2.rs
@@ -1,18 +1,56 @@
+use itertools::Itertools;
+use rayon::prelude::*;
+use std::collections::HashMap;
+
 pub fn solve(input: &str) -> usize {
-    input.lines().count()
+    input
+        .par_lines()
+        .map(get_sequences)
+        .reduce_with(|mut a, mut b| {
+            for (k, v) in b.drain() {
+                *a.entry(k).or_default() += v;
+            }
+            a
+        })
+        .unwrap()
+        .into_values()
+        .max()
+        .unwrap()
+}
+
+fn get_sequences(line: &str) -> HashMap<[i8; 4], usize> {
+    let mut secret_number: usize = line.parse().unwrap();
+    let mut last_digit = (secret_number % 10) as i8;
+    (0..2000)
+        .map(move |_| {
+            secret_number ^= secret_number << 6;
+            secret_number &= (1 << 24) - 1;
+            secret_number ^= secret_number >> 5;
+            //secret_number &= (1 << 24) - 1;
+            secret_number ^= secret_number << 11;
+            secret_number &= (1 << 24) - 1;
+            let prev = last_digit;
+            let next = (secret_number % 10) as i8;
+            last_digit = next;
+            (prev - next, next)
+        })
+        .tuple_windows()
+        .fold(HashMap::new(), |mut acc, (a, b, c, d)| {
+            acc.entry([a.0, b.0, c.0, d.0]).or_insert(d.1 as usize);
+            acc
+        })
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = include_str!("../example.txt");
+    const EXAMPLE: &str = include_str!("../example2.txt");
 
     #[test]
-    #[ignore = "not done yet"]
     fn example() {
         let result = solve(EXAMPLE);
-        assert_eq!(result, 42);
+        assert_eq!(result, 23);
     }
 
     #[cfg(input_txt)]
